d41b87727fb40757dcb86cfbcd79d96702fbeef3
J4r0d-x55 <108584837+J4r0d-x55@users.noreply.github.com>
2023-09-12 09:16:40 +0200

    Add files via upload

---
+++ LastLogon.ps1
@@ -0,0 +1,28 @@
+<#Description : Script permettant d'afficher les LastLogon de tous les utilisateurs de l'AD
+Auteur : Julien Borri
+Date de création : 11/09/2023
+Version : 1.0
+#>
+
+Import-Module ImportExcel
+Import-Module ActiveDirectory
+
+$utilisateurs = Get-ADUser -Filter * -Properties lastLogon
+
+$resultats = @()
+
+foreach ($utilisateur in $utilisateurs) {
+    $nomUtilisateur = $utilisateur.SamAccountName
+    $lastLogon = [DateTime]::FromFileTime($utilisateur.lastLogon)
+
+    $ligneFormattee = [PSCustomObject]@{
+        "Nom d'utilisateur" = $nomUtilisateur
+        "LastLogon" = $lastLogon.ToString("yyyy-MM-dd HH:mm:ss")
+    }
+
+    $resultats += $ligneFormattee
+}
+
+$resultats | Export-Excel -Path "C:\Users\adm-jbo\Desktop\lastLogon_AD.xlsx" -AutoSize -AutoFilter
+
+Write-Host "Les dates de lastLogon de tous les utilisateurs de l'Active Directory ont été exportées dans un fichier Excel"
